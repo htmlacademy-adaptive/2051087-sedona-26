2a4567de6d33981a3da86e4f49fdf64e095fb626
Nikita <tereshkonikita@mail.ru>
2022-09-10 19:07:00 +0300

    Запускает сборку и устанавливает пакеты минификации css

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,22 @@ import plumber from 'gulp-plumber';
 import less from 'gulp-less';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
+import csso from 'postcss-csso';
+import rename from 'gulp-rename';
 import browser from 'browser-sync';
 
 // Styles
 
 export const styles = () => {
-  return gulp.src('source/less/style.less', { sourcemaps: true })
-    .pipe(plumber())
-    .pipe(less())
-    .pipe(postcss([
-      autoprefixer()
+  return gulp.src('source/less/style.less', { sourcemaps: true }) // 1 состояние - найди файл
+    .pipe(plumber()) // обработка ошибок
+    .pipe(less()) // style.less -> style.css (библиотека gulp-less)
+    .pipe(postcss([ // библиотека автопрефиксов (autoprefixer)
+      autoprefixer(),
+      csso() // получаем минифицированный файл css
     ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('source/css', { sourcemaps: '.' })) // 3 состояние - куда положить файл
     .pipe(browser.stream());
 }
 
