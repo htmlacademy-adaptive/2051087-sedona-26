bddd7ad55b1548066af922279224bcbeb43dd83f
Nikita <tereshkonikita@mail.ru>
2022-09-10 23:45:23 +0300

    Добавляет часть тасок для сборки

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,12 @@ import autoprefixer from 'autoprefixer';
 import csso from 'postcss-csso';
 import rename from 'gulp-rename';
 import browser from 'browser-sync';
+import htmlmin from 'gulp-htmlmin';
+// import del from 'del';
+import squosh from 'gulp-libsquoosh';
+// import svgstore from 'gulp-svgstore';
+import webpo from 'gulp-webp';
+import svgo from 'gulp-svgmin';
 
 // Styles
 
@@ -18,16 +24,57 @@ export const styles = () => {
       csso() // получаем минифицированный файл css
     ]))
     .pipe(rename('style.min.css'))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' })) // 3 состояние - куда положить файл
+    .pipe(gulp.dest('build/css', { sourcemaps: '.' })) // 3 состояние - куда положить файл
     .pipe(browser.stream());
 }
+
+// Создание сборки (HTML)
+export const html = () => {
+  return gulp.src('source/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('build'));
+}
+
+export const libsquo = () => {
+  return gulp.src(['source/img/catalog/*.jpg', '!source/img/catalog/*.png', '!source/img/index/*.jpg', '!source/img/index/*.png', '!source/img/vaficon/*.jpg', '!source/img/vaficon/*.png'])
+  .pipe(webpo())
+  .pipe(gulp.dest('build/img'));
+}
+
+export const webp = () => {
+  return gulp.src(['source/img/catalog/*.jpg', '!source/img/catalog/*.png', '!source/img/index/*.jpg', '!source/img/index/*.png', '!source/img/vaficon/*.jpg', '!source/img/vaficon/*.png'])
+  .pipe(squosh())
+  .pipe(gulp.dest('build/webP'));
+}
+
+export const svg = () => {
+  return gulp.src(['source/img/catalog/*.svg', '!source/img/index/*.svg', '!source/img/vaficon/*.svg', '!source/img/footer/*.svg'])
+  .pipe(svgo())
+  .pipe(gulp.dest('build/svg'));
+}
+
+// export const dele = () => {
+//   return del('build');
+// };
+
+// Scripts
+export const script = () => {
+  return gulp.src('source/js/*.js')
+    .pipe(terser())
+    .pipe(gulp.dest('build/js'));
+}
+// Images
+
+// WebP
+
+// SVG
 
 // Server
 
 const server = (done) => {
   browser.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -45,5 +92,5 @@ const watcher = () => {
 
 
 export default gulp.series(
-  styles, server, watcher
+  svg, webp, libsquo, script, html, styles, server, watcher
 );
